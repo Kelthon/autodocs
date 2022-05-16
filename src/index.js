2058528a163a825eef43412e6e918a09b574e919
Kelthon <kelthonbalbino@gmail.com>
2022-05-16 16:53:27 -0300

    new tcc 1 route added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const res = require("express/lib/response");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send("Auto Docs");
+})
+
+app.post('/new/tcc/1/', (req, res) => {
+    const { title, student, professor } = req.body;
+    res.status(200).json({
+        title: title,
+        student: student,
+        professor: professor,
+    });
 })
 
 app.listen(port, () => {
