470662841220e7c578a64d84f57fdaf18912cdf1
Kelthon <kelthonbalbino@gmail.com>
2022-05-18 15:37:08 -0300

    doc model 1 implemented

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const pdf = require("pdfkit");
+const path = require("path");
 const fs = require("fs");
 const app = express();
 const port = 8080;
-
+    
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
@@ -12,19 +13,39 @@ app.get('/', (req, res) => {
 })
 
 app.post('/new/doc/1/', (req, res) => {
-    const { title, student, professor } = req.body;
+    const { matricula, student, professor, title, period, coord, ass, siape } = req.body;
 
-    const doc = new pdf();
-
-    doc.text(title);
-    doc.text(student);
-    doc.text(professor);
+    const doc = new pdf({
+        size: "A4",
+        font: "Times-Roman",
+    });
+    
+    doc.fontSize(12).lineGap(9)
+        .font("Times-Bold").text(
+            "DECLARAÇÃO", 
+            { align: "center", }
+        )
+        .moveDown().moveDown()
+        .font("Times-Roman").text(
+            "Declaro para os devidos fins que ",
+            { align: "justify", continued: true })
+        .font("Times-Bold").text(`${professor}`, { continued: true })
+        .font("Times-Roman").text(", participou na condição de membro da Banca Examinadora do Trabalho de Conclusão de Curso I, de Graduação em Engenharia Civil, do(a) discente ", { continued: true })
+        .font("Times-Italic").text(`${student}`, { continued: true })
+        .font("Times-Roman").text(", com título ", { continued: true })
+        .font("Times-Bold").text(`${title}`, { continued: true })
+        .font("Times-Roman").text(`, referente ao período ${period}`, { continued: true })
+        .moveDown().moveDown().moveDown()
+        .text("Juazeiro do Norte,", { align: "right" })
+        .text(`${ass}`, { align: "center", })
+        .text(`${coord}`, { align: "center", })
+        .text(`Siape: ${siape}`, { align: "center", })
 
     doc.end();
 
-    doc.pipe(fs.createWriteStream("./file.pdf"));
+    doc.pipe(fs.createWriteStream(`./declaracao-tcc-1-${matricula}.pdf`));
     doc.pipe(res);
-    res.status(200).sendFile("./file.pdf");
+    res.status(200).sendFile(`./declaracao-tcc-1-${matricula}.pdf`);
 })
 
 app.listen(port, () => {
