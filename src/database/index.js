bc1c65e349a857eb4cd64f087cf2f4fdc7d49b69
Kelthon <kelthonbalbino@gmail.com>
2022-06-10 09:59:22 -0300

    refactored database index

--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,16 @@ const sequelize = new Sequelize(db);
 try {
     sequelize.authenticate();
     console.log("DB was connected successfully!");
-} catch {
-    console.log("failure to connect DB!");
+
+    try {
+        sequelize.sync({ force: true });
+        console.log("All models were synchronized successfully.");
+    } catch(err) {
+        console.log("failure to sync models to DB!\n" + err);
+    }
+
+} catch(err) {
+    console.log("failure to connect DB!\n" + err);
 }
 
 module.exports = sequelize;
