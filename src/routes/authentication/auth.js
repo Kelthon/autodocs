d35d26754373b0ac85a5cbb39a68e35dcd45b67c
Kelthon <kelthonbalbino@gmail.com>
2022-06-10 10:02:00 -0300

    added auth route

--- src/routes/authentication/auth.js
+++ src/routes/authentication/auth.js
@@ -1,19 +1,38 @@
-const router = require("express").Router();
-const user = require("../models/user");
+const express = require("express");
+const loginStatus = require("../../middlewares/login");
+const User = require("../../models/user");
+const { isSiape } = require("../../utils/validator");
+const router = express.Router();
 
-router.use();
+router.post("/login", loginStatus.isNotLogged, (req, res) => {
+    let stat, msg;
+    const { account } = req.body;
+    console.log(req.originalUrl, req.url)
 
-router.post("/login", (req, res) => {
-    let msg;
-    let stat;
-    const siape = req.body.siape;
+    console.log(req.body, account, isSiape(account));
     
-    if(user.findOne({siape: siape}) == null) {
-        stat = 404
-        msg = "User not found!";
+    if(isSiape(account) == true) {
+        try {
+            const user = User.findOne({ where: { siape: account }});
+            req.session.isLogged = true;
+            stat = 200;
+            msg = "You was logged in!";
+        } catch(err) {
+            stat = 404;
+            msg = "User not found!";
+        }
+    } else {
+        stat = 404;
+        msg = "Invalid Siape";
     }
-
-    res.status(stat).json({
-        message: msg
-    });
+        res.status(stat).json({
+            message: msg
+    })
 });
+
+router.get("/logout", loginStatus.isLogged, (req, res) => {
+    req.session.isLogged = false;
+    res.status(204).send();
+});
+
+module.exports = router;
