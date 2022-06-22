5df8ed155b72e232986350868230821c39f82a8a
Kelthon <kelthonbalbino@gmail.com>
2022-06-22 13:33:13 -0300

    modularized functions

---
+++ src/routes/new/doc.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { isLogged } = require("../../middlewares/login");
+const { tcc1 } = require("../../middlewares/tcc1");
+
+const router  = express.Router();
+
+router.post('/new/doc/:id', isLogged, tcc1, (req, res) => {
+    res.status(204).send();
+})
+
+module.exports = router;
