88f8773d2b995fb71acae807e9869e7e7e4fb47d
Kelthon <kelthonbalbino@gmail.com>
2022-07-06 14:00:38 -0300

    new pages

---
+++ client/src/pages/Doclist.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+function Doclist() {
+    return(
+        // <Link to="/new/doc/tcc">Novo documento de TCC 1</Link>
+        <div>
+            <h1>Lista</h1>
+        </div>
+    );
+}
+
+export default Doclist;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Login() {
+    return(
+        <div>
+            <h1>Login</h1>
+            {/* <form method="POST">
+                <label>Siape</label>
+                <input type={}/>
+            </form> */
+            }
+        </div>
+    );
+}
+
+export default Login
