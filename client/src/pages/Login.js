9f14622c5ae618056f439962534c213ad1a4f3b8
Kelthon <kelthonbalbino@gmail.com>
2022-07-08 09:24:03 -0300

    form updated

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate} from "react-router-dom";
+import {
+    Button,
+    Form,
+    Container,
+} from "react-bootstrap"
+
+import api from "../services/api"
 
 function Login() {
-    return(
-        <div>
-            <h1>Login</h1>
-            {/* <form method="POST">
-                <label>Siape</label>
-                <input type={}/>
-            </form> */
+    
+    const [siape, setSiape] = React.useState();
+    const [logged, setLogged] = React.useState();
+    const navigate = useNavigate()
+
+    const onSiape = ({target: {value}}) => setSiape(value)
+    const onSubmitForm = event => {
+        event.preventDefault()
+        
+        api.post("/login", { account: siape }).then(res => {
+            if(res.status == 200) {
+                setLogged(true);
             }
-        </div>
+        })
+            
+        onSiape();
+    }
+    
+    useEffect(() => {
+        if(logged) {
+            return navigate("/");
+        }
+    }, []);
+
+    return(
+        <Container>
+            <h1 className="mt-4">Login</h1>
+            <Form onSubmit={onSubmitForm}>
+                <Form.Group className="mb-3" controlId="formSiape">
+                    <Form.Label>Siape</Form.Label>
+                    <Form.Control onChange={onSiape} value={siape} type="text" placeholder="Siape" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Não sou um robô" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Entrar
+                </Button>
+            </Form>
+        </Container>
     );
 }
 
