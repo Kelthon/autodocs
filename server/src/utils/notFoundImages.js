02b1d7ddee6d1158db9b05bff7c129e1f1d04f05
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-07-21 12:06:33 -0300

    public folder not exist error fixed

--- server/src/utils/notFoundImages.js
+++ server/src/utils/notFoundImages.js
@@ -1,20 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-const images = [
-    "Assinatura Horizontal Principal.png", 
-    "Brasão Principal.png",
-    "Brasão Vertical Preto.png", 
-    "ondas.png", "UFCA+CCT CH (2).png", 
-    "UFCA+CCT CV.png", "UFCA+CCT RH.png", 
-    "UFCA+CCT RV.png", 
-    "table_av.png"
-];
-
-const imagesFolder = path.join(__dirname, "..", "..", "public", "img");
-
 module.exports = () => {
+    const publicFolder =  path.join(__dirname, "..", "..", "public")
+    const imagesFolder = path.join(publicFolder, "img");
     const err = []; let notFound = false;
+    
+    const images = [
+        "Assinatura Horizontal Principal.png", 
+        "Brasão Principal.png",
+        "Brasão Vertical Preto.png", 
+        "ondas.png", "UFCA+CCT CH (2).png", 
+        "UFCA+CCT CV.png", "UFCA+CCT RH.png", 
+        "UFCA+CCT RV.png", 
+        "table_av.png"
+    ];
+
+    try {
+        if(!fs.existsSync(path.join(__dirname, "..", "..", "public"))) {
+            fs.mkdirSync(publicFolder);
+            fs.mkdirSync(imagesFolder);
+        }
+    } catch(err) {
+        err.push("failure on create public directory");
+        return {exist: notFound, error: err};
+    }
 
     images.forEach(filename => {
         if (!fs.existsSync(path.join(imagesFolder, filename))) {
