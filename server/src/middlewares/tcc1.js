1cfb384944e88ef469a9f86143707bc37ba3b54b
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-07-21 12:08:10 -0300

    refactored

--- server/src/middlewares/tcc1.js
+++ server/src/middlewares/tcc1.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const Coordinator = require("../models/coordinator");
 const User = require("../models/user");
 const docFormAv = require("../utils/formAvaliacao");
@@ -6,7 +7,7 @@ const docDecOrientador = require("../utils/declaracaoOrientador");
 const date = require("../utils/date");
 
 module.exports = {
-    tcc1: function (req, res, next) {
+    tcc1: async function (req, res, next) {
         const {
             projectTitle, 
             studentName,
@@ -14,94 +15,112 @@ module.exports = {
             studentPeriod, 
             secondMemberName,
             secondMemberTitle,
+            professorName,
+            professorTitle,
+            professorEmail,
             thirdMember,
             thirdMemberName,
             thirdMemberTitle,
             presentationRoom, 
             presentationDate, 
             presentationHour,
-            coordinatorName, 
-            jobTitle,
         } = req.body;
 
-        if(!req.session.name || !req.session.title || !req.session.email) {
-            console.log(req.session)
-            return res.status(400).json({
-                message: "Invalid session"
-            });
-        }
+        const errors = [], docs = []; 
+        let coordinatorName, coordinatorSignature, coordinatorSiape, jobTitle;
+        let searchFor;
 
-        const   professorName = req.body.name, 
-                professorTitle = req.body.title, 
-                professorEmail = req.session.email;
-        
-        const errors = [], docs = [];
-        const coordinator = Coordinator.findOne({ where: { id: 1 } });
-        const userCoordinator = User.findOne({ where: { id: coordinator.coordinator } });
-        const coordinatorSignature = coordinator.coordinatorSignaturePath; 
-        const coordinatorSiape = userCoordinator.siape;
+        const coordinator = await Coordinator.findByPk(1).then(async coord => {
+            if(coord.active) {
+                jobTitle = "Coordenador"; 
+                searchFor = coord.coordinator;
+            } else {
+                jobTitle = "Vicecoordenador";
+                searchFor = coord.viceCoordinator;
+            }
+            
+            const userCoordinator = await User.findByPk(searchFor).then(user => {
+                coordinatorName = user.name;
+                coordinatorSignature = coord.coordinatorSignaturePath; 
+                coordinatorSiape = user.siape;
+            }).catch(err => {
+                errors.push(err.message);
+                console.log(err);
+            });
+        }).catch(err => {
+            errors.push(err.message);
+            console.log(err);
+        });     
+
         const filenames = [
             `${date("YYYY-MM-DD")}-${studentRegistration} Declaração do Segundo Membro da Banca.pdf`,
             `${date("YYYY-MM-DD")}-${studentRegistration} Formulário de Avaliação.pdf`,
             `${date("YYYY-MM-DD")}-${studentRegistration} Declaração do Orientador.pdf`,
             `${date("YYYY-MM-DD")}-${studentRegistration} Declaração do Membro da Banca.pdf` 
         ];
-
-        docFormAv(
-            filenames[0],
-            projectTitle,
-            professorName,
-            professorTitle,
-            studentName,
-            secondMemberName,
-            secondMemberTitle,
-            thirdMember,
-            thirdMemberName,
-            thirdMemberTitle,
-            presentationRoom,
-            presentationDate,
-            presentationHour
-        );
-
-        docDecOrientador(
-            filenames[1],
-            projectTitle, 
-            professorName, 
-            studentName, 
-            studentPeriod, 
-            coordinatorName, 
-            coordinatorSignature, 
-            coordinatorSiape,
-            jobTitle
-        );
-
-        docDecMember(
-            filenames[2],
-            projectTitle, 
-            professorName, 
-            studentName,
-            studentPeriod, 
-            coordinatorName, 
-            coordinatorSignature, 
-            coordinatorSiape,
-            jobTitle,
-        );
-
-        if(thirdMember == 'true') docDecMember(
-            filenames[3],
-            projectTitle, 
-            professorName, 
-            studentName,
-            studentPeriod, 
-            coordinatorName, 
-            coordinatorSignature, 
-            coordinatorSiape,
-            jobTitle,
-        );
+                
+        try {
+            docs.push(docFormAv(
+                filenames[0],
+                projectTitle,
+                professorName,
+                professorTitle,
+                studentName,
+                secondMemberName,
+                secondMemberTitle,
+                thirdMember,
+                thirdMemberName,
+                thirdMemberTitle,
+                presentationRoom,
+                presentationDate,
+                presentationHour
+            ));
+            
+            docs.push(docDecOrientador(
+                filenames[1],
+                projectTitle, 
+                professorName, 
+                studentName, 
+                studentPeriod, 
+                coordinatorName, 
+                coordinatorSignature, 
+                coordinatorSiape,
+                jobTitle
+            ));
+                
+            docs.push(docDecMember(
+                filenames[2],
+                projectTitle, 
+                secondMemberName, 
+                studentName,
+                studentPeriod, 
+                coordinatorName, 
+                coordinatorSignature, 
+                coordinatorSiape,
+                jobTitle,
+            ));
+            
+            if(thirdMember == 'true' || thirdMember == 'on') 
+                docs.push(docDecMember(
+                    filenames[3],
+                    projectTitle, 
+                    thirdMemberName, 
+                    studentName,
+                    studentPeriod, 
+                    coordinatorName, 
+                    coordinatorSignature, 
+                    coordinatorSiape,
+                    jobTitle,
+                ));
+        } catch(err) {
+            console.log(err);
+            errors.push(err.message);
+        }
 
         if(errors.length > 0) {
             docs.forEach(element => fs.unlinkSync(element));
             return res.status(500).json({ errors: errors });
-        } else next();
+        } else 
+        next();
     }
 }
