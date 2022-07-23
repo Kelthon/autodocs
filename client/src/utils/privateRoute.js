544a4e2cdbc3aef18adfc0eff645472a1577c4e8
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-07-23 16:15:02 -0300

    private routes created

---
+++ client/src/utils/privateRoute.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../contexts/authcontext";
+
+const PrivateRoute = ({children}) => {
+    const { authenticated, loading } = useContext(AuthContext);
+    if(loading) {
+        return <div className="loading">loading...</div>
+    }
+    return authenticated ? children : <Navigate to="/login"/>
+}
+
+export default PrivateRoute;
