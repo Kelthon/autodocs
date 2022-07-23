bec8c29a6992b1a8c333c9916db8319950078582
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-07-23 16:13:12 -0300

    updated

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Home() {
+function HomePage() {
+
     return(
         <div>
             <h1>Início</h1>
             <Link to="/new/doc" >Criar novo documento</Link><br/>
-            <Link to="/login" >Entrar</Link>
+            <Link to="/logout" >Sair</Link><br/>
         </div>
     );
 }
 
-export default Home;
+export default HomePage;
