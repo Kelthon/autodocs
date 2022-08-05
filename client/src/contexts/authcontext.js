3fd88bd4cf2fb7efc5aab642deef3efc6cb52c1d
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-08-04 21:15:23 -0300

    login fixed

--- client/src/contexts/authcontext.js
+++ client/src/contexts/authcontext.js
@@ -1,41 +1,48 @@
-import React, { useState, createContext, useEffect } from "react"
+import  React, { useState, useRef, useEffect, createContext, useContext } from "react"
 import { useNavigate } from "react-router-dom";
 
-export const AuthContext = createContext({
-    authenticated: false, user: null, loading: true, login: (id, token) => {}, logout: () => {}
-});
+export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const navigate = useNavigate();
-    const [user, setUser] = useState(null);
+    const user = useRef(null);
     const [loading, setLoading] = useState(true);
     
     useEffect(() => {
         const storageUser = localStorage.getItem("user");
         
         if(storageUser) {
-            setUser(JSON.parse(storageUser));
+            user.current = JSON.parse(storageUser)
         }
 
         setLoading(false);
     }, []);
     
     const login = (id, token) => {
-        setUser({id: id, token: token});
+        user.current = {id: id, token: token}
         localStorage.setItem("user", JSON.stringify(user));
-        return navigate(-1);
+        return navigate(-2);
     }
     
     const logout = () => {
-        setUser(null);
-        return navigate("/");
+        user.current = null;
+        localStorage.removeItem("user")
+        return navigate("/", { replace: true });
     }
 
     return(
         <AuthContext.Provider
-            value={{ authenticated: !!user, user, loading, login, logout }}
+            value={{ authenticated: !!user.current, user, loading, login, logout }}
         >
             {children}
         </AuthContext.Provider>
     )
 };
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    
+    if(context === undefined) throw new Error("useAuth must be used within a AuthProvider");
+    
+    return context;
+}
