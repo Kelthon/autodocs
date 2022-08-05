a3e58a1006b38368c2a797c331e5d076c3157e3b
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-08-04 21:14:31 -0300

    AuthProvider removed

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,23 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import TcciFormPage from "./pages/tccFormPage";
-import { AuthProvider } from "./contexts/authcontext";
+
 import PrivateRoute from "./utils/privateRoute";
-import LogoutPage from "./pages/logoutPage";
 
 function RouterList() {
     
     return (
-        <AuthProvider>
-            <Routes>
-                <Route path="/" element={ <HomePage/>}/>
-                <Route path="login" element={ <LoginPage/> }/>
-                <Route path="logout" element={ <LogoutPage/> }/>
-                <Route path="new">
-                    <Route path="doc" element={<PrivateRoute><TcciFormPage/></PrivateRoute>}/>
-                </Route>
-            </Routes>
-        </AuthProvider>
+        <Routes>
+            <Route path="/" element={ <HomePage/>}/>
+            <Route path="login" element={ <LoginPage/> }/>
+            <Route path="new">
+                <Route path="doc" element={<PrivateRoute><TcciFormPage/></PrivateRoute>}/>
+            </Route>
+        </Routes>
     )
 }
 
