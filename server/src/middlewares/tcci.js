979c4908426755781cd85da17c060e770c04c06f
Kelthon <kelthon.alves@aluno.ufca.edu.br>
2022-08-15 19:49:20 -0300

    changes

--- server/src/middlewares/tcci.js
+++ server/src/middlewares/tcci.js
@@ -7,20 +7,18 @@ const docFormAv = require("../utils/formAvaliacao");
 const docDecMember = require("../utils/declaracaoMember");
 const docDecOrientador = require("../utils/declaracaoOrientador");
 const date = require("../utils/date");
-const doc = require("pdfkit");
 
 module.exports = {
     tcc1: async function (req, res, next) {
-        const {
+        const 
+        {
             projectTitle, 
             studentName,
             studentRegistration,
             studentPeriod, 
             secondMemberName,
             secondMemberTitle,
-            professorName,
-            professorTitle,
-            professorEmail,
+            professorId,
             thirdMember,
             thirdMemberName,
             thirdMemberTitle,
@@ -28,12 +26,19 @@ module.exports = {
             presentationDate, 
             presentationHour,
         } = req.body;
+        
+        const errors = [], 
+                docs = []; 
+        let coordinatorName,
+            coordinatorSignature,
+            coordinatorSiape,
+            jobTitle,
+            professorName,
+            professorTitle,
+            professorEmail,
+            searchFor;
 
-        const errors = [], docs = []; 
-        let coordinatorName, coordinatorSignature, coordinatorSiape, jobTitle;
-        let searchFor;
-
-        const coordinator = await Coordinator.findByPk(1).then(async coord => {
+        await Coordinator.findByPk(1).then(async coord => {
             if(coord.active) {
                 jobTitle = "Coordenador"; 
                 searchFor = coord.coordinator;
@@ -42,19 +47,29 @@ module.exports = {
                 searchFor = coord.viceCoordinator;
             }
             
-            const userCoordinator = await User.findByPk(searchFor).then(user => {
+            await User.findByPk(searchFor).then(user => {
                 coordinatorName = user.name;
                 coordinatorSignature = coord.coordinatorSignaturePath; 
                 coordinatorSiape = user.siape;
             }).catch(err => {
-                errors.push(err.message);
+                errors.push("An Error occorred to try find user coordinator");
                 console.log(err);
             });
         }).catch(err => {
-            errors.push(err.message);
+            errors.push("An Error occorred to try find coordinator");
             console.log(err);
         });     
-
+        
+        await User.findByPk(professorId).then(user => {
+            professorName = user.name;
+            professorTitle = user.jobTitle;
+            professorEmail = user.email;
+        }).catch(err => {
+            errors.push("An Error occorred to try find your user");
+            console.log(err);
+            return res.status(400).json({errors: errors})
+        })
+        
         const filenames = [
             `${date("YYYY-MM-DD")}-${studentRegistration} Declaração do Segundo Membro da Banca.pdf`,
             `${date("YYYY-MM-DD")}-${studentRegistration} Formulário de Avaliação.pdf`,
@@ -117,36 +132,41 @@ module.exports = {
                 ));
         } catch(err) {
             console.log(err);
-            errors.push(err.message);
+            errors.push("An Error occorred to try create files");
         }
 
         if(errors.length > 0) {
-            docs.forEach(element => fs.unlinkSync(element));
+            docs.forEach(element => {
+                if(fs.existsSync(element)) fs.unlinkSync(element)
+            });
             return res.status(500).json({ errors: errors });
         } else {
-            const account = await nodemailer.createTestAccount();
 
             const transpoter = nodemailer.createTransport({
-                host: "smtp.ethereal.email",
+                host: "smtp.gmail.com",
                 port: 587,
-                secure: false,
+                secure: true,
                 auth: {
-                    user: account.user,
-                    pass: account.pass,
+                    type: "OAuth2",
+                    user: "",
+                    accessToken: "",
+                    clientId: "",
+                    clientSecret: "",
+                    refreshToken: "",
+                    expires: "",
                 },
             });
 
-            const fileslist = []
-            for(let i = 0; i > filenames.length; i++) {
-                fileslist.push({filename: filenames[i], path:doc[i]})
-            }
-
-            const email = await transpoter.sendMail({
+            await transpoter.sendMail({
                 from: "autodocs@example.com",
-                to: `${professorName}, <${professorEmail}>`,
+                to: `kellth, <kellthgamer@gmail.com>`,
                 subject: "solicitação de documentos",
                 html: '<h1>Olá fulano!</h1><p>Segue anexado a este email os documentos gerados automaticamente pelo <a href="http://localhost:3000/">autodocs</a></p><strong>Não responda essa mesagem</strong>',
-                attachment: fileslist
+                attachment: { path: docs }
+            }).catch(err => {
+                errors.push("An Error occorred to try sent email")
+                console.log(err)
+                return res.status(500).json({errors: errors})
             }).finally(() => docs.forEach(element => fs.unlinkSync(element)));
         }
         next();
