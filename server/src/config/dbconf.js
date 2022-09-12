f52c1a1961b2334124be66c9c4475fc9a7604294
Kelthon <kelthonbalbino@gmail.com>
2022-09-12 08:49:43 -0300

    merge

--- server/src/config/dbconf.js
+++ server/src/config/dbconf.js
@@ -2,17 +2,10 @@ require("dotenv/config");
 
 module.exports = {
     dialect: "postgres",
-<<<<<<< HEAD
-    host: "postgres", 
-    username: "autodocs",
-    password: "I8Iknx_IUoZPOarjxhgkIboe-CPHz16J",
-    database: "autodocs",
-=======
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
->>>>>>> release/patch
     define: {
         timestamps: true,
         underscored: true, 
